1a707bde8f8afa3a6e517db9a21d527c1f747b6f
gempesaw <gempesaw@gmail.com>
2014-01-17 08:24:35 -0500

    Add yeoman generated Files service
    yo angular:service Files

---
+++ app/scripts/services/files.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('honeydewApp')
+  .service('Files', function Files() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ test/spec/services/files.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: Files', function () {
+
+  // load the service's module
+  beforeEach(module('honeydewApp'));
+
+  // instantiate service
+  var Files;
+  beforeEach(inject(function (_Files_) {
+    Files = _Files_;
+  }));
+
+  it('should do something', function () {
+    expect(!!Files).toBe(true);
+  });
+
+});

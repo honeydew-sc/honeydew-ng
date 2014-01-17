b43cacb071880dd708018f0d61b8900c24608032
gempesaw <gempesaw@gmail.com>
2014-01-17 06:54:21 -0500

    Initial commit
    Output of:
    
    ```
    yo angular honeydew
    bower install angular-ui-codemirror --save
    ```

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('honeydewApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

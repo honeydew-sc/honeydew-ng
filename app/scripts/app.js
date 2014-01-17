a78eb5739f739283a1f869bed28397a83680f120
gempesaw <gempesaw@gmail.com>
2014-01-17 08:23:12 -0500

    Implement code mirror to feature view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,23 @@
 'use strict';
 
 angular.module('honeydewApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ui.codemirror'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/feature', {
-        templateUrl: 'views/feature.html',
-        controller: 'FeatureCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/feature/:feature*', {
+                templateUrl: 'views/feature.html',
+                controller: 'FeatureCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/feature.js
+++ app/scripts/controllers/feature.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('honeydewApp')
-  .controller('FeatureCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('FeatureCtrl', [
+        '$scope',
+        '$routeParams',
+        function ($scope, $routeParams) {
+
+            $scope.display = function( file ) {
+                // $scope.contents = file;
+                $scope.feature = Files.get( { file: encodeURIComponent(file) }, function (res) {
+                    $scope.contents = res.contents;
+                });
+            };
+
+            // put feature contents in model if it's in URL
+            if ($routeParams.feature) {
+                $scope.display($routeParams.feature);
+            }
+        }]);

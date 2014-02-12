16fb61326b8f183a4ce1ef8a21c8180e39f9febf
gempesaw <gempesaw@gmail.com>
2014-02-12 15:42:37 -0500

    Add connect proxy so livereload is useful

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost',
+                hostname: '0.0.0.0',
                 livereload: 35729
             },
             livereload: {
@@ -73,7 +73,26 @@ module.exports = function (grunt) {
                     base: [
                         '.tmp',
                         '<%= yeoman.app %>'
-                    ]
+                    ],
+                    middleware: function (connect, options) {
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
+
+                        // Setup the proxy
+                        var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+                        // Serve static files.
+                        options.base.forEach(function(base) {
+                            middlewares.push(connect.static(base));
+                        });
+
+                        // Make directory browse-able.
+                        var directory = options.directory || options.base[options.base.length - 1];
+                        middlewares.push(connect.directory(directory));
+
+                        return middlewares;
+                    }
                 }
             },
             test: {
@@ -90,7 +109,16 @@ module.exports = function (grunt) {
                 options: {
                     base: '<%= yeoman.dist %>'
                 }
-            }
+            },
+            proxies: [
+                {
+                    context: ['/rest.php', '/reports.php', '/docs'],
+                    host: 'localhost',
+                    port: 80,
+                    https: false,
+                    changeOrigin: false
+                }
+            ]
         },
 
         // Make sure code styles are up to par and there are no obvious mistakes
@@ -354,6 +382,8 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-connect-proxy');
+
 
     grunt.registerTask('serve', function (target) {
         if (target === 'dist') {
@@ -362,6 +392,7 @@ module.exports = function (grunt) {
 
         grunt.task.run([
             'clean:server',
+            'configureProxies:server',
             'bower-install',
             'concurrent:server',
             'autoprefixer',

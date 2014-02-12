247444ea2d667954b9fb162e233a2e6dd054e67b
gempesaw <gempesaw@gmail.com>
2014-02-12 15:42:10 -0500

    Fix scroll to for pages

--- app/scripts/common/directives/scrollTo.js
+++ app/scripts/common/directives/scrollTo.js
@@ -28,39 +28,18 @@ angular.module('honeydew')
         return {
             restrict : "AC",
             compile : function(){
-
                 var document = $window.document;
 
-                function scrollInto(idOrName, offset) {//find element with the given id or name and scroll to the first element it finds
-                    if(!idOrName) //move to top if idOrName is not provided
-                        $window.scrollTo(0, 0);
-
-                    //check if an element can be found with id attribute
-                    var el = document.getElementById(idOrName);
-                    if(!el) {//check if an element can be found with name attribute if there is no such id
-                        el = document.getElementsByName(idOrName);
-
-                        if(el && el.length)
-                            el = el[0];
-                        else
-                            el = null;
-                    }
-
-                    if(el) { //if an element is found, scroll to the element
-                        if (offset) {
-                            var top = $(el).offset().top - offset;
-                            window.scrollTo(0, top);
-                        }
-                        else {
-                            el.scrollIntoView();
-                        }
-                    }
-                    //otherwise, ignore
+                function scrollInto(idOrName) {//find element with the given id or name and scroll to the first element it finds
+                    var el = Array.prototype.slice.call(document.getElementsByName(idOrName));
+                    el.forEach(function (it) {
+                        it.scrollIntoView(true);
+                    });
                 }
 
                 return function(scope, element, attr) {
                     element.bind("click", function(event){
-                        scrollInto(attr.scrollTo, attr.offset);
+                        scrollInto(attr.scrollTo);
                     });
                 };
             }

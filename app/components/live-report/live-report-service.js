bd02c5310396dba34c72f295c5cfe7415e460268
gempesaw <gempesaw@gmail.com>
2014-03-24 23:55:59 -0400

    Add five minute idle timeout to live report service

--- app/components/live-report/live-report-service.js
+++ app/components/live-report/live-report-service.js
@@ -16,10 +16,11 @@ angular.module('honeydew')
         getPusherToken.send();
     })
 
-    .service('liveReport', function ($rootScope, Pusher, randomString) {
+    .service('liveReport', function ($rootScope, Pusher, randomString, $timeout) {
+        var timeout;
         var service =  {
-            oldChannel: '',
-            channel: '',
+            oldChannel: null,
+            channel: null,
             pusherChannel: null,
             output: 'There\'s nothing to see here yet; try executing a feature! :)',
             placeHolder: true,
@@ -27,19 +28,23 @@ angular.module('honeydew')
             events: {
                 evalRule: 'client-eval-rule',
                 finish: 'client-finish'
-            }
+            },
+            autoVacateDelay: 5 * 60 * 1000
         };
+
 
         service.switchChannel = function (channel) {
             if (typeof(channel) === 'undefined') {
                 var newChannel = 'private-' + randomString.string();
                 return service.switchChannel(newChannel);
             }
-
-            service.oldChannel = service.channel;
-            service.channel =  channel;
-            service.tail(service.channel);
-            return service.channel;
+            else {
+                service.close();
+                service.oldChannel = service.channel;
+                service.channel = channel;
+                service.tail(service.channel);
+                return service.channel;
+            }
         };
 
         service.pusherListener = function (item) {
@@ -54,25 +59,38 @@ angular.module('honeydew')
 
         service.tail = function (channel) {
             // we don't want input from two channels at once
-            if (service.oldChannel !== '') {
-                try {
-                    Pusher.unsubscribe(service.oldChannel);
-                }
-                catch (e) {
-                    console.log('sorry, couldn\'t unsub: ', e);
-                };
+            if (service.oldChannel) {
+                Pusher.unsubscribe(service.oldChannel);
             }
 
             // need a placeholder while the job starts
             service.output = 'Loading...' + "\n";
             service.placeHolder = true;
 
-            Pusher.subscribe(channel, 'rule',  service.pusherListener);
+            // we don't have a slick way of knowing when to close the
+            // connection; for now they're on an idle auto-close timer
+            Pusher.subscribe(channel, 'rule',  service.pusherListener).then(function (channel) {
+                service.pusherChannel = channel;
+                timeout = $timeout( service.close, service.autoVacateDelay);
+            });
         };
 
         service.evalRule = function (rule) {
-            if (typeof(Pusher.channel) !== 'undefined') {
-                Pusher.channel.trigger(service.events.evalRule, rule.trim());
+            if (typeof(service.pusherChannel) !== 'undefined') {
+                $timeout.cancel(timeout);
+                timeout = $timeout( service.close, service.autoVacateDelay);
+
+                service.pusherChannel.trigger(service.events.evalRule, rule.trim());
+            }
+        };
+
+        service.close = function () {
+            if (service.pusherChannel) {
+                service.pusherChannel.trigger(service.events.finish, service.events.finish);
+            }
+
+            if (timeout) {
+                $timeout.cancel(timeout);
             }
         };
 
--- app/components/live-report/live-report-service_test.js
+++ app/components/live-report/live-report-service_test.js
@@ -1,15 +1,28 @@
 'use strict';
 
 describe('LiveReportService', function () {
-    var liveReportService, rootScope, pusherMock;
+    var liveReportService, rootScope, pusherMock, timeout, pusherReal;
     var channel = 'testChannel';
     var rule = 'Given I am on the / page';
     beforeEach(module('honeydew'));
 
-    beforeEach(inject(function (_liveReport_, _Pusher_) {
+    beforeEach(inject(function (_liveReport_, _Pusher_, $timeout) {
         liveReportService = _liveReport_;
-        pusherMock = _Pusher_;
-        spyOn(pusherMock, 'subscribe');
+        pusherMock = pusherReal = _Pusher_;
+        pusherMock.channel = {
+            trigger: function () {}
+        };
+        spyOn(pusherMock.channel, 'trigger');
+
+        timeout = $timeout;
+        spyOn(pusherMock, 'subscribe').andCallFake(function (params) {
+            return {
+                then: function (cb) {
+                    cb(pusherMock.channel);
+                }
+            };
+        });
+
         spyOn(pusherMock, 'unsubscribe');
     }));
 
@@ -41,12 +54,30 @@ describe('LiveReportService', function () {
     });
 
     it('sends messages in private channels', function () {
-        pusherMock.channel = {
-            trigger: function () {}
-        };
-        spyOn(pusherMock.channel, 'trigger');
-
+        liveReportService.switchChannel();
         liveReportService.evalRule(rule);
         expect(pusherMock.channel.trigger).toHaveBeenCalledWith(liveReportService.events.evalRule, rule);
     });
+
+    it('closes itself if we join a new channel and immediately idle', function () {
+        spyOn(liveReportService, 'close');
+
+        liveReportService.switchChannel();
+        expect(liveReportService.close).toHaveBeenCalled();
+        timeout.flush();
+        expect(liveReportService.close.callCount).toBe(2);
+    });
+
+    it('can we idle after evaluating a rule', function () {
+        spyOn(liveReportService, 'close');
+
+        liveReportService.switchChannel();
+        liveReportService.close.reset();
+        liveReportService.evalRule('blah blah blah');
+        expect(liveReportService.close).not.toHaveBeenCalled();
+        timeout.flush();
+        expect(liveReportService.close.callCount).toBe(1);
+        expect(liveReportService.close).toHaveBeenCalled();
+    });
+
 });

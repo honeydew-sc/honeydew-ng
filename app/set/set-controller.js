d48f9327227093efdfa2713087402d2c3492102a
Daniel Gempesaw <gempesaw@gmail.com>
2014-06-05 11:49:09 -0400

    Set set history grid alongside included features

--- app/set/set-controller.js
+++ app/set/set-controller.js
@@ -1,11 +1,43 @@
 'use strict';
 
 angular.module('honeydew')
-    .controller('SetCtrl', function ($scope, $stateParams, Files, alerts) {
-        var filename = 'sets/' + $stateParams.set;
-        $scope.file = Files.get({file: Files.encode(filename)}, function ( res ) {
-            console.log(res);
-        }, alerts.catcher);
+    .controller('SetCtrl', function ($scope, $stateParams, Files, alerts, SetReport) {
+        $scope.editorOptions = {
+            lineWrapping : false,
+            lineNumbers: true,
+            readOnly: true
+        };
 
-        console.log($stateParams);
+        var filename = 'sets/' + $stateParams.set;
+        $scope.file = Files.get({file: Files.encode(filename)}, function () {}, alerts.catcher);
+
+        $scope.setHistory = SetReport.get({ name: $stateParams.set }, function () { }, alerts.catcher);
+        console.log($scope.setHistory);
+
+        $scope.gridOptions = {
+            data: 'setHistory.report',
+            columnDefs: [
+                {
+                    field: 'start',
+                    displayName: 'Start Date',
+                    cellFilter: 'date:\'MM/dd/yyyy\''
+                },
+                {
+                    field: 'host',
+                    displayName: 'Host'
+                },
+                {
+                    field: 'browser',
+                    displayName: 'Browser'
+                },
+                {
+                    field: 'status',
+                    displayName: 'Status'
+                }
+            ],
+            multiSelect: false,
+            enableRowSelection: true
+        };
+
+
     });

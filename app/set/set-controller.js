c56fc46049f517da422f0f3c4c0d912094501bcd
Daniel Gempesaw <gempesaw@gmail.com>
2014-06-06 14:32:55 -0400

    Make scrollbar visible and codemirror unselectable for sets

--- app/set/set-controller.js
+++ app/set/set-controller.js
@@ -5,7 +5,7 @@ angular.module('honeydew')
         $scope.editorOptions = {
             lineWrapping : false,
             lineNumbers: true,
-            readOnly: true,
+            readOnly: "nocursor",
             mode: 'set-mode',
             onLoad: function (cm) {
                 CmDomHelpers.focus(cm, $scope);

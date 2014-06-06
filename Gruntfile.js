0449148d172960180f97e783fe2dce1e44fb5419
Daniel Gempesaw <gempesaw@gmail.com>
2014-06-06 19:12:54 -0400

    Include webm files in the dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,7 +180,7 @@ module.exports = function (grunt) {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webm,webp,svg}',
                         '<%= yeoman.dist %>/styles/fonts/*'
                     ]
                 }
@@ -212,7 +212,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>/images',
-                    src: '{,*/}*.{png,jpg,jpeg,gif}',
+                    src: '{,*/}*.{png,jpg,jpeg,gif,webm}',
                     dest: '<%= yeoman.dist %>/images'
                 }]
             }

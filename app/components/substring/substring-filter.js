ba88e51bd00562a7441f3219a96b864b6caaca5a
Daniel Gempesaw <gempesaw@gmail.com>
2014-06-06 18:33:38 -0400

    Add substring filter as shortcut to substring() fn

---
+++ app/components/substring/substring-filter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// courtesy of https://gist.github.com/mugifly/4942965
+angular.module('honeydew')
+    .filter('substring', function() {
+        return function(str, start, end) {
+            return str.substring(start, end);
+        };
+    });

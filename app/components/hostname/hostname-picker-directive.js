5b35546991c9648b339ad33a04951b876139d6f4
Daniel Gempesaw <gempesaw@gmail.com>
2014-08-05 18:10:42 -0400

    Switch Hostname dir to ControllerAs syntax

--- app/components/hostname/hostname-picker-directive.js
+++ app/components/hostname/hostname-picker-directive.js
@@ -5,27 +5,27 @@ angular.module('sc.hostname')
         return {
             template:
             '    <div class="hostname-group">' +
-                '  <div class="input-group" ng-class="{ open: open}">' +
-                '    <input class="form-control hostname" name="host" ng-model="hostname.host" required placeholder="http://www.sharecare.com" ng-click="open = false">' +
+                '  <div class="input-group" ng-class="{ open: Host.open}">' +
+                '    <input class="form-control hostname" name="host" ng-model="Host.name.host" required placeholder="http://www.sharecare.com" ng-click="Host.open = false">' +
                 '    <label class="sr-only" for="host">Hostname</label>' +
-                '    <button type="button" class="btn btn-default" ng-click="open = !open">' +
+                '    <button type="button" class="btn btn-default" ng-click="Host.open = !Host.open">' +
                 '      <span class="caret"></span>' +
                 '      <span class="sr-only">Toggle Dropdown</span>' +
                 '    </button>' +
                 '    <ul class="dropdown-menu" role="menu">' +
-                '      <button type="button" class="close" ng-click="open = false">' +
+                '      <button type="button" class="close" ng-click="Host.open = false">' +
                 '        <span aria-hidden="true">&times;</span>' +
                 '        <span class="sr-only">Close</span>' +
                 '      </button>' +
                 '      <li>' +
                 '        <div class="btn-group">' +
-                '          <button ng-repeat="app in hostname.appOptions" type="button" class="btn btn-hdew app" ng-model="hostname.app" btn-radio="app">{{ app }}</button>' +
+                '          <button ng-repeat="app in Host.name.appOptions" type="button" class="btn btn-hdew app" ng-model="Host.name.app" btn-radio="app" ng-click="Host.emit(app, undefined)">{{ app }}</button>' +
                 '        </div>' +
                 '      </li>' +
                 '      <li class="divider"></li>' +
                 '      <li>' +
                 '        <div class="btn-group">' +
-                '          <button ng-repeat="env in hostname.envOptions" type="button" class="btn btn-hdew env" ng-model="hostname.env" btn-radio="env" ng-click="$parent.open = false">{{ env }}</button>' +
+                '          <button ng-repeat="env in Host.name.envOptions" type="button" class="btn btn-hdew env" ng-model="Host.name.env" btn-radio="env" ng-click="Host.emit(undefined, env)">{{ env }}</button>' +
                 '        </div>' +
                 '      </li>' +
                 '    </ul>' +
@@ -34,8 +34,10 @@ angular.module('sc.hostname')
             replace: true,
             scope: true,
             restrict: 'E',
-            link: function ( scope, element, attrs ) {
-                scope.hostname = hostname;
-            }
+            controller: function ( $scope, hostname ) {
+                var self = this;
+                self.name = hostname;
+            },
+            controllerAs: 'Host'
         };
     });

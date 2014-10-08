255593ad91dad9d2858cea5ba99f2e4e67188ec5
Daniel Gempesaw <gempesaw@gmail.com>
2014-10-08 10:38:43 -0400

    Add keywords to autocomplete source JSON response

--- backend/rest-routes/autocomplete-sources.php
+++ backend/rest-routes/autocomplete-sources.php
@@ -5,12 +5,19 @@ $app->group('/autocomplete', function () use ($app) {
     $app->get('/', function () {
         $rules = json_decode(file_get_contents('rest-routes/sources/rules.json'));
         $phrases = json_decode(file_get_contents('rest-routes/sources/phrases.json'));
+
+        /* as a reminder, this file is included in rest.php, which
+        handles the inclusion of hdewdb_connect.php, where in this fn
+        is defined. */
+        $keywords = getKeyWords();
+
         exec('/usr/bin/perl -w /opt/honeydew/bin/parseRules.pl > /dev/null 2>&1 &');
         exec('/usr/bin/perl -w /opt/honeydew/bin/parsePhrases.pl > /dev/null 2>&1 &');
         echo json_encode(array(
             'suggestRules' => $rules[0],
             'regexRules' => $rules[1],
-            'phrases' => $phrases
+            'phrases' => $phrases,
+            'keywords' => $keywords
         ));
     });
 });

035b5c7f7fc229c4b492c1f391029892fe7587ac
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-16 23:33:05 -0500

    Finish available browser service tests

--- app/components/available-browsers/available-browsers-service_test.es6.js
+++ app/components/available-browsers/available-browsers-service_test.es6.js
@@ -1,15 +1,16 @@
 'use strict';
 
-ddescribe('availableBrowsersService', () => {
-    var availableBrowsers;
+describe('availableBrowsersService', () => {
+    var availableBrowsers, $location;
 
     beforeEach(module('honeydew'));
 
-    beforeEach(inject( function ( _availableBrowsers_ ) {
+    beforeEach(inject( function ( _availableBrowsers_, _$location_ ) {
         availableBrowsers = _availableBrowsers_;
+        $location = _$location_;
     }));
 
-    it('shoulve have a couple browser choices', () => {
+    it('should have a couple browser choices', () => {
         var browsers = availableBrowsers.getBrowsers();
 
         expect(browsers.length).toBeGreaterThan(4);
@@ -19,6 +20,13 @@ ddescribe('availableBrowsersService', () => {
         var servers = availableBrowsers.getServers();
 
         expect(servers.length).toBeGreaterThan(5);
+        expect(servers).toContain('Localhost');
     });
 
+    it('should not let monitor users see localhost', () => {
+        spyOn($location, 'path').and.returnValue('something/monitor/something');
+        var servers = availableBrowsers.getServers();
+
+        expect(servers).not.toContain('Localhost');
+    });
 });

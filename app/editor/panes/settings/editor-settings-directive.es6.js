f6c338fe77e267eea9f3bae7633c126626f0b8c5
Daniel Gempesaw <gempesaw@gmail.com>
2014-12-22 15:43:52 -0500

    Refactor to controller as syntax in editor-settings directive

--- app/editor/panes/settings/editor-settings-directive.es6.js
+++ app/editor/panes/settings/editor-settings-directive.es6.js
@@ -1,63 +1,70 @@
-'use strict';
+(() => {
+    'use strict';
 
-angular.module('honeydew')
-    .directive('editorSettings', function ($localStorage) {
-        return {
-            templateUrl: 'editor/panes/settings/editor-settings.html',
-            scope: {
-                options: '='
-            },
-            replace: true,
-            restrict: 'E',
-            controller: function ($scope) {
-                $scope.$storage = $localStorage.$default({
-                    settings: {
-                        font: 'Default',
-                        theme: 'xq-light',
-                        collapse: {
-                            tree: 'false',
-                            execute: 'false'
-                        }
-                    }
-                });
-
-                $scope.settingsOptions = {
-                    fonts: [
-                        'Default',
-                        'Carl'
-                    ],
-
-                    themes: [
-                        '3024-day',
-                        '3024-night',
-                        'ambiance',
-                        'base16-dark',
-                        'base16-light',
-                        'blackboard',
-                        'cobalt',
-                        'default',
-                        'eclipse',
-                        'elegant',
-                        'erlang-dark',
-                        'lesser-dark',
-                        'mbo',
-                        'mdn-like',
-                        'midnight',
-                        'monokai',
-                        'neat',
-                        'night',
-                        'paraiso-dark',
-                        'paraiso-light',
-                        'rubyblue',
-                        'solarized',
-                        'the-matrix',
-                        'tomorrow-night-eighties',
-                        'twilight',
-                        'vibrant-ink',
-                        'xq-dark',
-                        'xq-light'
-                    ]
-                };
+    function EditorSettingsCtrl($scope, $localStorage) {
+        this.$storage = $localStorage.$default({
+            settings: {
+                font: 'Default',
+                theme: 'xq-light',
+                collapse: {
+                    tree: 'false',
+                    execute: 'false'
+                }
             }
+        });
+
+        this.options = {
+            fonts: [
+                'Default',
+                'Carl'
+            ],
+
+            themes: [
+                '3024-day',
+                '3024-night',
+                'ambiance',
+                'base16-dark',
+                'base16-light',
+                'blackboard',
+                'cobalt',
+                'default',
+                'eclipse',
+                'elegant',
+                'erlang-dark',
+                'lesser-dark',
+                'mbo',
+                'mdn-like',
+                'midnight',
+                'monokai',
+                'neat',
+                'night',
+                'paraiso-dark',
+                'paraiso-light',
+                'rubyblue',
+                'solarized',
+                'the-matrix',
+                'tomorrow-night-eighties',
+                'twilight',
+                'vibrant-ink',
+                'xq-dark',
+                'xq-light'
+            ]
         };
-    });
+
+        this.redrawPage = () => {
+            $scope.$emit('codemirror:redraw');
+        };
+    }
+
+    angular.module('honeydew')
+        .directive('editorSettings', function () {
+            return {
+                templateUrl: 'editor/panes/settings/editor-settings.html',
+                replace: true,
+                restrict: 'E',
+                controller: 'EditorSettingsCtrl'
+            };
+        })
+        .controller('EditorSettingsCtrl', EditorSettingsCtrl);
+
+})();

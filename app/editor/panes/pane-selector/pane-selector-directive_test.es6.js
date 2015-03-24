9b8ad3d8113c94854305d70627031487d70b1c62
Daniel Gempesaw <gempesaw@gmail.com>
2015-03-24 09:09:00 -0400

    Initialize local storage for pane selector tests for travis

--- app/editor/panes/pane-selector/pane-selector-directive_test.es6.js
+++ app/editor/panes/pane-selector/pane-selector-directive_test.es6.js
@@ -3,18 +3,25 @@
 describe('Pane Selector directive', () => {
     var elm,
         scope,
-        compile;
+        compile,
+        paneScope;
 
     beforeEach(module('honeydew'));
     beforeEach(module('tpl'));
 
-    beforeEach(inject( ($compile, $rootScope) => {
+    beforeEach(inject( ($compile, $rootScope, $localStorage) => {
         elm = angular.element('<pane-selector></pane-selector');
         scope = $rootScope;
         compile = $compile;
 
+        // initialize some localstorage settings for the various panes
+        // to depend on
+        $localStorage.settings = { theme: '' };
+
         compile(elm)(scope);
         scope.$digest();
+
+        paneScope = elm.scope();
     }));
 
     it('should do repeat through the different panes', () => {
@@ -23,18 +30,18 @@ describe('Pane Selector directive', () => {
     });
 
     it('should reset a report status', () => {
-        elm.scope().report.failure = 'failure';
+        paneScope.report.failure = 'failure';
         scope.$broadcast('report:reset');
         scope.$digest();
-        expect(elm.scope().report.failure).toBe('');
+        expect(paneScope.report.failure).toBe('');
         expect(elm.find('.active.failure').length).toBe(0);
     });
 
     it('should reset a report status', () => {
-        elm.scope().report.failure = '';
+        paneScope.report.failure = '';
         scope.$broadcast('report:failure');
         scope.$digest();
-        expect(elm.scope().report.failure).toBe('failure');
+        expect(paneScope.report.failure).toBe('failure');
         expect(elm.find('.active.failure').length).toBe(1);
     });
 });

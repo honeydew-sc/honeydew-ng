c28c0023095340fd9c4da1c48c98b9557fc12449
Daniel Gempesaw <gempesaw@gmail.com>
2015-05-18 22:35:00 -0400

    Revert "Go scorched earth with ngAnnotate"
    This reverts commit bd428d350089dcf58bad001c6abe3bfb59235047; it slows
    the feedback loop down too much to have to annotate every js file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
                 files: [ '<%= yeoman.app %>/**/*.es6' ],
                 tasks: [
                     'newer:traceur',
-                    'ngAnnotate:watch',
+                    'newer:ngAnnotate:watch',
                     'newer:uglify:watch'
                 ]
             },

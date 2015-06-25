cc9ecef3a57003cfd02ae762224aabf0b674c44c
Daniel Gempesaw <gempesaw@gmail.com>
2015-06-25 12:11:09 -0400

    Add dependencies as expressed in previous commit

---
+++ app/bower_components/system.js/test/tests/testpkg/test.js
@@ -0,0 +1,7 @@
+System.registerModule("app/bower_components/system.js/test/tests/testpkg/test.ts", [], function() {
+  "use strict";
+  var __moduleName = "app/bower_components/system.js/test/tests/testpkg/test.ts";
+  module.exports = 'ts';
+  return {};
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsS0FBSyxlQUFlLEFBQUMsNkRBQW9CLEdBQUMsQ0FBMUMsVUFBUyxBQUFEOztBQUFSLEFBQUksSUFBQSxDQUFBLFlBQVcsOERBQW9CLENBQUM7QUFBcEMsT0FBSyxRQUFRLEVBQUksS0FBRyxDQUFDO0FBQXJCLFdBQXVCO0FBRWpCLENBRndELENBQUM7QUFBMUMiLCJmaWxlIjoiYXBwL2Jvd2VyX2NvbXBvbmVudHMvc3lzdGVtLmpzL3Rlc3QvdGVzdHMvdGVzdHBrZy90ZXN0LmpzIiwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSAndHMnOyJdfQ==
---
+++ app/bower_components/system.js/test/tests/typescript.js
@@ -0,0 +1,7 @@
+System.registerModule("app/bower_components/system.js/test/tests/typescript.ts", [], function() {
+  "use strict";
+  var __moduleName = "app/bower_components/system.js/test/tests/typescript.ts";
+  module.exports = 'typescript';
+  return {};
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInR5cGVzY3JpcHQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsS0FBSyxlQUFlLEFBQUMsMkRBQW9CLEdBQUMsQ0FBMUMsVUFBUyxBQUFEOztBQUFSLEFBQUksSUFBQSxDQUFBLFlBQVcsNERBQW9CLENBQUM7QUFBcEMsT0FBSyxRQUFRLEVBQUksYUFBVyxDQUFDO0FBQTdCLFdBQXVCO0FBRWpCLENBRndELENBQUM7QUFBbEMiLCJmaWxlIjoiYXBwL2Jvd2VyX2NvbXBvbmVudHMvc3lzdGVtLmpzL3Rlc3QvdGVzdHMvdHlwZXNjcmlwdC5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gJ3R5cGVzY3JpcHQnOyJdfQ==

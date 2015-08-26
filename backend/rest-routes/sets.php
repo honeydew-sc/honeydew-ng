18fea45daee589cf96b1ec78aeb55edc0490ce24
Daniel Gempesaw <gempesaw@gmail.com>
2015-08-26 18:21:43 -0400

    Implement set-rename backend functionality

--- backend/rest-routes/sets.php
+++ backend/rest-routes/sets.php
@@ -27,4 +27,44 @@ $app->group('/sets', function () use ($app, $setsDir) {
             "setCount" => $count
         ));
     });
+
+    /* rename an existing set */
+    $app->post('/:setFilename', function ( $setFilename ) use ( $app ) {
+        try {
+            /* resolveFilename expects an exploded array of file path
+            parts */
+
+            $oldPath = resolveFilename( array( 'sets', $setFilename ) );
+            $contents = refreshSet( $oldPath );
+            $features = array_filter( explode("\n", $contents), 'strlen' );
+
+            $oldShortSetName = preg_replace( '/\.set$/', '', $setFilename );
+            $newShortSetName = json_decode( $app->request()->getBody() )->newSetName;
+
+            renameSet( $oldShortSetName, $newShortSetName, $features );
+
+            unlink( $oldPath );
+            echo successMessage(array( 'newName' => $newShortSetName ) );
+        }
+        catch ( Exception $e ) {
+            $app->halt(418, errorMessage("Set rename error: " . $e->getMessage()));
+        }
+    });
+
+    function renameSet( $old, $new, $features ) {
+        if ( preg_match( '/[^A-z0-9-._]/', $new ) ) {
+            throw new Exception( 'The new filename is funky?: ' . $new );
+        }
+
+        $rewrite_command_base = 'perl -wpi -e \'s/(Set(?::|: |:.* ))\@' . $old . '($| )/$1\@' . $new . '$2/\'';
+
+        $config = get_config();
+        $hd_features_base = $config['honeydew']['basedir'] . 'features/';
+
+        $features_arg = implode( ' ', array_map( function( $it ) use ( $hd_features_base ) {
+            return $hd_features_base . $it;
+        }, $features ) );
+        $rewrite_cmd = $rewrite_command_base . ' ' . $features_arg;
+        return exec($rewrite_cmd);
+    }
 });
--- backend/tests/sets_tests.php
+++ backend/tests/sets_tests.php
@@ -5,8 +5,30 @@ require(dirname(__FILE__) . './../vendor/autoload.php');
 require_once(dirname(__FILE__) . './../vendor/simpletest/simpletest/autorun.php');
 
 class filesTests extends UnitTestCase {
+    protected $base = 'http://localhost/rest.php';
     protected $baseUrl = "http://localhost/rest.php/sets";
     protected $basePath = "/opt/honeydew";
+
+    private $names = array( 'fake.feature', 'fake2.feature' );
+    function setupFakeSet() {
+        $features_dir = $this->basePath . '/features/';
+        $contents = "Feature: hello\nSet: @haystack @needle @okay\n\nScenario: yes okay";
+
+        $names = $this->names;
+        array_walk( $names, function ( $it ) use ( $features_dir, $contents ) {
+            $filename = $features_dir . $it;
+            unlink( $filename );
+            file_put_contents( $filename, $contents );
+        });
+    }
+
+    function cleanupFakeSet() {
+        $names = $this->names;
+        array_walk( $names, function ( $it ) use ( $features_dir, $contents ) {
+            $filename = $features_dir . $it;
+            unlink( $filename );
+        });
+    }
 
     function testGet() {
         $response = \Httpful\Request::get($this->baseUrl)->send();
@@ -14,5 +36,72 @@ class filesTests extends UnitTestCase {
         $this->assertFalse( in_array( '.set', $response->body->sets ) );
     }
 
+    function testRenameExistingSet() {
+        $this->setupFakeSet();
 
+        /* rename @needle to @renamed */
+        $response = \Httpful\Request::post($this->baseUrl . '/needle.set')
+             ->body(json_encode(array( 'newSetName' => 'renamed' )))
+        /* ->withoutAutoParsing() */
+             ->send();
+
+        /* check that @renamed exists */
+        $response = \Httpful\Request::get($this->base . '/files/sets/renamed.set')
+             ->send();
+        $this->assertPattern( '/fake.feature\s+fake2.feature/', $response->body->contents );
+
+        /* check that @needle is gone */
+        $response = \Httpful\Request::get($this->base . '/files/sets/needle.set')
+             ->send();
+        $this->assertEqual( 404, $response->code );
+        $this->assertPattern( '/No such file or directory/', $response->body->reason );
+
+        $this->cleanupFakeSet();
+    }
+
+    function testInvalidNewRenames() {
+        $invalid = array(
+            '!@#$%',
+            'renamed)',
+
+        );
+
+        foreach ( $invalid as $newName ) {
+            $this->setupFakeSet();
+
+            $response = \Httpful\Request::post($this->baseUrl . '/needle.set')
+                 ->body(json_encode(array( 'newSetName' => $newName )))
+                 ->send();
+
+            $this->assertPattern(
+                '/Set rename error: The new filename is funky/',
+                $response->body->reason,
+                'cannot rename to ' . $newName
+            );
+
+            $this->cleanupFakeSet();
+        }
+
+    }
+
+    function testValidNewRenames() {
+        $valid = array(
+            'reNAMED',
+            'superm_-.n',
+            're_na-ME-ed90'
+        );
+
+        foreach ( $invalid as $newName ) {
+            $this->setupFakeSet();
+
+            $response = \Httpful\Request::post($this->baseUrl . '/needle.set')
+                 ->body(json_encode(array( 'newSetName' => $newName )))
+                 ->send();
+
+            $this->assertEqual( 200, $response->code, 'can rename to ' . $newName);
+
+            $this->cleanupFakeSet();
+        }
+
+    }
 }
